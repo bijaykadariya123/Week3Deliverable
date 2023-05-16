ffb7c88cae9d089dea3aecd2013cc1e5dc5b278f
Bijay Kadariya <bijaykadariya@bijays-mbp.myfiosgateway.com>
2023-05-16 11:18:04 -0400

    addidng id containter for div element

--- hogwarts/app.js
+++ hogwarts/app.js
@@ -1,5 +1,10 @@
+/*
 if (typeof jQuery == "undefined") {
   console.log("oops! I stil have to link my jQuery properly!");
 } else {
   console.log("I did it! I linked jQuery and this js file!");
 }
+*/
+//----------------------------------Year 1----------------------------------------------------/
+const $container = document.querySelector("body");
+console.log($container);
