a759a21aa02d7daf5b5e4a7ae8bd6161b8917461
Bijay Kadariya <bijaykadariya@bijays-mbp.myfiosgateway.com>
2023-05-16 14:25:41 -0400

    Commit 2 - I added more elements. My power grows with my dedication and hard work

--- hogwarts/app.js
+++ hogwarts/app.js
@@ -10,3 +10,15 @@ const $container = $("#container");
 // console.log($container);
 const $h1 = $("<h1>").html("Hogwarts");
 $container.append($h1);
+
+const $h2 = $("<h2>");
+$h2.html("Bijay");
+$container.append($h2);
+const $h3 = $("<h3>").html("RavenClaw");
+$container.append($h3);
+const $h4 = $("<h4>").addClass("pet").html("owl");
+$container.append($h4);
+const $h42 = $("<h4>").html(
+  "If You Were To Enter The Magical World Of Harry Potter Which Villon Would You Be?"
+);
+$container.append($h42);
