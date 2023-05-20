79061779f6a8c14f6281d6298e51c1cb71d700f3
Bijay Kadariya <bijaykadariya92@gmail.com>
2023-05-20 05:48:55 -0400

    Commit 3 - I added list elements. That code didn't seem very dry. I wonder if there is something I could do about it in the Hungry for More section

--- hogwarts/app.js
+++ hogwarts/app.js
@@ -10,7 +10,7 @@ const $container = $("#container");
 // console.log($container);
 const $h1 = $("<h1>").html("Hogwarts");
 $container.append($h1);
-
+// ==================================================//
 const $h2 = $("<h2>");
 $h2.html("Bijay");
 $container.append($h2);
@@ -19,20 +19,26 @@ $container.append($h3);
 const $h4 = $("<h4>").addClass("pet").html("owl");
 $container.append($h4);
 const $h42 = $("<h4>").html(
-  "If You Were To Enter The Magical World Of Harry Potter Which Villon Would You Be?"
+  "If You Were To Enter The Magical World Of Harry Potter Which Villain Would You Be?"
 );
 $container.append($h42);
 
-const $ul = $("<ul>").attr("storage", "trunk");
-$container.append($ul);
-const $li = $("<li>").html("butter beer");
-$ul.append($li);
-const $li1 = $("<li>")
-  .addClass("secret")
-  .html("invisibility cloak")
-  .addClass("secret")
-  .html("magic map")
-  .addClass("secret")
-  .html("time turner");
+//===================================================//
 
-$ul.append($li1);
+const $ul = $("<ul>").attr("storage", "trunk");
+
+let $li = $("<li>").html("butter beer");
+$ul.append($li);
+// $li = $("<li>").addClass("secret");
+// $ul.append($li);
+$li = $("<li>").addClass("secret").html("invisibility cloak");
+$ul.append($li);
+$li = $("<li>").addClass("secret").html("magic map");
+$ul.append($li);
+$li = $("<li>").addClass("secret").html("time turner");
+$ul.append($li);
+$li = $("<li>").addClass("pet").html("leash");
+$ul.append($li);
+$li = $("<li>").html(`Bertie Bott\'s Every Flavor [Jelly] Beans`);
+$ul.append($li);
+$container.append($ul);
